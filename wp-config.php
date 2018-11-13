9ee9ab45d0eccdc642e39e7c0ec8a0e40ea6792b
Alexander <al.yakubovich@yahoo.com>
2018-11-13 20:04:15 +0300

    13Nov18

--- wp-config.php
+++ wp-config.php
@@ -89,3 +89,6 @@ define('CONCATENATE_SCRIPTS', false);
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_HOME','http://example.com');
+define('WP_SITEURL','http://example.com');
---
+++ wp-content/themes/fordummies/public/wp-config.php
@@ -0,0 +1,2 @@
+define('WP_HOME','http://example.com');
+define('WP_SITEURL','http://example.com');
